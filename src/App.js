10451d24dbc2c788386809330813f5189bc7f380
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-10-03 15:40:01 +0600

    initially completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import User from "./pages/User/User";
 import Admin from "./pages/Admin/Admin";
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://volunteernetwork-akash.herokuapp.com",
+  baseURL: "https://volunteernetwork-akash.herokuapp.com",
 });
 
 export default instance;
